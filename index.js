aacd904000460ea71ce2e05c85ff1f16e88a9579
MUSTIADO <farhanmaalim6@gmail.com>
2024-03-30 17:40:24 +0300

    feat:phase-1-javascript-functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(opt="roller-skate"){
+    return `This Saturday, I want to ${opt}!`;
+}
+SaturdayFun("bathe my dog");
+
+function mondayWork(opt="go to the office"){
+    return `This Monday, I will ${opt}.`;
+
+}
+
+function wrapAdjective(adjective="*"){
+    const innerFunction=function (parameter="special"){
+        return `You are ${adjective}${parameter}${adjective}!`;
+    };
+    return innerFunction
+}
